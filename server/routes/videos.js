27e24e6253879752037421bd9221399ada7c1a51
ilovelili <route666@live.cn>
2015-12-20 23:40:58 +0900

    set default mongo server

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -2,23 +2,23 @@
  * API layer
  */
 var MongoClient = require('mongodb').MongoClient,    
-    host = process.env.MongoHost || '127.0.0.1',
+    host = process.env.MongoHost || '45.55.130.170',
     port = process.env.MongoPort || 27017,
     db = 'ero',
     url = 'mongodb://{{host}}:{{port}}/{{db}}'.replace('{{host}}', host).replace('{{port}}', port).replace('{{db}}', db),
     ObjectId = require('mongodb').ObjectID;
 
-exports.findById = function(req, res) {
+exports.findById = function (req, res) {
     'use strict';
     var id = req.params.id;
     console.log('findById: ' + id);
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         if (!err) {
             var col = db.collection('videos');
             col.findOne({
                 _id: new ObjectId(id),
                 enabled: true
-            }, function(err, doc) {
+            }, function (err, doc) {
                 if (!err) {
                     res.jsonp(doc);
                 }
@@ -28,18 +28,18 @@ exports.findById = function(req, res) {
     });
 };
 
-exports.findByCat = function(req, res) {
+exports.findByCat = function (req, res) {
     'use strict';
     var cat = req.params.cat;
     console.log('findByCat: ' + cat);
 
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         if (!err) {
             var col = db.collection('videos');
             col.find({
                 category: cat,
                 enabled: true
-            }).toArray(function(err, docs) {
+            }).toArray(function (err, docs) {
                 if (!err) {
                     res.jsonp(docs);
                 }
@@ -49,14 +49,14 @@ exports.findByCat = function(req, res) {
     });
 };
 
-exports.findAll = function(req, res) {
+exports.findAll = function (req, res) {
     'use strict';
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         if (!err) {
             var col = db.collection('videos');
             col.find({
                 enabled: true
-            }).toArray(function(err, docs) {
+            }).toArray(function (err, docs) {
                 if (!err) {
                     res.jsonp(docs);
                 }
