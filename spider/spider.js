de82912f0c4ec8084758578b037aa7e0d818cd22
ilovelili <route666@live.cn>
2016-02-21 11:46:34 -0500

    change verbose to debug

--- spider/spider.js
+++ spider/spider.js
@@ -4,7 +4,7 @@ var links = [],
     fs = require('fs'),
     casper = require('casper').create({
         verbose: true,
-        logLevel: 'error',
+        logLevel: 'debug',
         pageSettings: {
             loadImages: false,
             loadPlugins: false
