7d0e384a8e8a06d1e95ba962e7a22ce8c6adceb8
ilovelili <route666@live.cn>
2016-03-01 10:35:05 +0900

    temp hack

--- client/lib/run.js
+++ client/lib/run.js
@@ -1,2 +1,16 @@
 $('.bs-component [data-toggle="popover"]').popover();
 $('.bs-component [data-toggle="tooltip"]').tooltip();
+
+// hack, delete me later
+var seed,
+    hackStyle = function() {
+    	// begin hack
+    	if($('.title.ng-binding').length) {
+    		$('.title.ng-binding').parent().css('height', '200px');
+    		$('.title.ng-binding').css('font-size', '20px');
+
+    		window.clearInterval(seed);
+    	}
+    };
+
+seed = window.setInterval(hackStyle, 300);
