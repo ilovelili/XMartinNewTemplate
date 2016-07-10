0a80f17ed16eca5ae297ec07c8712ec475b175d8
ilovelili <route666@live.cn>
2016-07-10 13:12:08 +0900

    set timeout to nativead directive

--- client/js/directive/nativeadspot.js
+++ client/js/directive/nativeadspot.js
@@ -1,24 +1,26 @@
 (function (angular) {
     'use strict';
     angular.module('eroMartin.nativeAdSpotDirective', []).directive('nativeAdSpot', NativeAdSpotFunc);
-    NativeAdSpotFunc.$inject = ['UseragentService'];
+    NativeAdSpotFunc.$inject = ['UseragentService', '$timeout'];
 
-    function NativeAdSpotFunc(UseragentService) {
+    function NativeAdSpotFunc(UseragentService, $timeout) {
         return {
-            restrict: 'E',            
+            restrict: 'E',
             templateUrl: 'partial/_nativeadspot.html',
             replace: true,
             // use parent scope
             scope: false,
             link: function (scope) {
-                scope.limit = scope.limit || /*(UseragentService.isPC ? 15 : 14)*/15;
-                scope.extendLimit = scope.extendLimit || function (event) {
-                    scope.limit += 8;
-                    event.preventDefault();
-                };
-                scope.overCap = scope.overCap || function (event) {
-                    return scope.videos && scope.limit >= scope.videos.length;
-                };
+                $timeout(function () {
+                    scope.limit = scope.limit || /*(UseragentService.isPC ? 15 : 14)*/15;
+                    scope.extendLimit = scope.extendLimit || function (event) {
+                        scope.limit += 8;
+                        event.preventDefault();
+                    };
+                    scope.overCap = scope.overCap || function (event) {
+                        return scope.videos && scope.limit >= scope.videos.length;
+                    };
+                }, 1000);
             },
         };
     }
