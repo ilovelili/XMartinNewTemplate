d457439a9813f41f8eed08d84d136d17dd43bc9b
ilovelili <route666@live.cn>
2016-07-10 13:14:45 +0900

    revert

--- client/js/directive/nativeadspot.js
+++ client/js/directive/nativeadspot.js
@@ -1,9 +1,9 @@
 (function (angular) {
     'use strict';
     angular.module('eroMartin.nativeAdSpotDirective', []).directive('nativeAdSpot', NativeAdSpotFunc);
-    NativeAdSpotFunc.$inject = ['UseragentService', '$timeout'];
+    NativeAdSpotFunc.$inject = ['UseragentService'];
 
-    function NativeAdSpotFunc(UseragentService, $timeout) {
+    function NativeAdSpotFunc(UseragentService) {
         return {
             restrict: 'E',
             templateUrl: 'partial/_nativeadspot.html',
@@ -11,16 +11,14 @@
             // use parent scope
             scope: false,
             link: function (scope) {
-                $timeout(function () {
-                    scope.limit = scope.limit || /*(UseragentService.isPC ? 15 : 14)*/15;
-                    scope.extendLimit = scope.extendLimit || function (event) {
-                        scope.limit += 8;
-                        event.preventDefault();
-                    };
-                    scope.overCap = scope.overCap || function (event) {
-                        return scope.videos && scope.limit >= scope.videos.length;
-                    };
-                }, 1000);
+                scope.limit = scope.limit || /*(UseragentService.isPC ? 15 : 14)*/15;
+                scope.extendLimit = scope.extendLimit || function (event) {
+                    scope.limit += 8;
+                    event.preventDefault();
+                };
+                scope.overCap = scope.overCap || function (event) {
+                    return scope.videos && scope.limit >= scope.videos.length;
+                };
             },
         };
     }
